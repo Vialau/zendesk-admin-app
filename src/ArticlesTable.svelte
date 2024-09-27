9589bd0623d7c46390aab06a78f26a2cf914e9a7
Vianney LAURENT <vianney.lau@gmail.com>
2024-09-27 19:10:35 +0200

    Update ArticlesTable.svelte
    Change API URL and add pagination

--- src/ArticlesTable.svelte
+++ src/ArticlesTable.svelte
@@ -1,204 +1,225 @@
 
 <script>
-    import { onMount } from 'svelte';
-    import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Assurez-vous d'avoir importé Bootstrap JS
-    let selectedCategory = '';
-    let selectedSection = '';
-    let labelFilter = 'all'; // 'all' ou 'duplicate'
+  import { onMount } from 'svelte';
+  import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Assurez-vous d'avoir importé Bootstrap JS
+  let selectedCategory = '';
+  let selectedSection = '';
+  let labelFilter = 'all'; // 'all' ou 'duplicate'
 
-    // Ceci est une variable pour stocker les articles venant de l'API.
-    let articles = [];
-    // Ceci est une variable pour stocker les sections venant de l'API.
-    let sectionNames = {}; // { section_id: "Section Name", ... }
-    let categoryNames = {}; // { category_id: "Category Name", ... }
+  // Ceci est une variable pour stocker les articles venant de l'API.
+  let articles = [];
+  // Ceci est une variable pour stocker les sections venant de l'API.
+  let sectionNames = {}; // { section_id: "Section Name", ... }
+  let categoryNames = {}; // { category_id: "Category Name", ... }
 
-    let showModal = false; // Contrôle l'affichage de la modal
-    let userEmail = ''; // Stocke l'email de l'utilisateur
-    let userPassword = ''; // Stocke le mot de passe de l'utilisateur
-    let encodedCredentials = ''; // Stocke les credentials encodées
+  let showModal = false; // Contrôle l'affichage de la modal
+  let userEmail = ''; // Stocke l'email de l'utilisateur
+  let userPassword = ''; // Stocke le mot de passe de l'utilisateur
+  let encodedCredentials = ''; // Stocke les credentials encodées
 
-    let labels = []; // Cet état contiendra tous les labels avec leur ID et leur nom
+  let labels = []; // Cet état contiendra tous les labels avec leur ID et leur nom
 
-    let labelToDelete = null;
+  let labelToDelete = null;
 
+  // Pour chaque article, vous aurez besoin d'un état pour stocker le nouveau label
+  let newLabels = {}; // { articleId: 'new label name' }
 
-    // Pour chaque article, vous aurez besoin d'un état pour stocker le nouveau label
-    let newLabels = {}; // { articleId: 'new label name' }
+  let currentPage = 1; // Page actuelle
+  const pageSize = 25; // Nombre d'articles par page
 
-    // Lors du chargement du composant, nous allons chercher les articles.
-    onMount(async () => {
-        if (!localStorage.getItem('encodedCredentials')) {
-            showModal = true; // Pas de credentials, montrez la modal
-        } else {
-            encodedCredentials = localStorage.getItem('encodedCredentials'); 
-            await loadArticles();  
-        }
-    });
-    
-    onMount(() => {
-    loadLabels(); // Appeler loadLabels lors de l'initialisation du composant
+  // Lors du chargement du composant, nous allons chercher les articles.
+  onMount(async () => {
+      if (!localStorage.getItem('encodedCredentials')) {
+          showModal = true; // Pas de credentials, montrez la modal
+      } else {
+          encodedCredentials = localStorage.getItem('encodedCredentials'); 
+          await loadArticles();  
+      }
+  });
+  
+  onMount(() => {
+      loadLabels(); // Appeler loadLabels lors de l'initialisation du composant
   });
 
-    var handleLogin = async function() {
-        // Encodez les credentials et stockez-les dans localStorage pour cet exemple
-        encodedCredentials = btoa(`${userEmail}:${userPassword}`);
-        localStorage.setItem('encodedCredentials', encodedCredentials);
+  var handleLogin = async function() {
+      // Encodez les credentials et stockez-les dans localStorage pour cet exemple
+      encodedCredentials = btoa(`${userEmail}:${userPassword}`);
+      localStorage.setItem('encodedCredentials', encodedCredentials);
 
-        // Fermez la modal après la connexion
-        showModal = false;
-        await loadArticles();
-    }
+      // Fermez la modal après la connexion
+      showModal = false;
+      await loadArticles();
+  }
 
-    var loadArticles = async function() {
-        const articlesResponse = await fetch('https://newenki.zendesk.com/api/v2/help_center/fr/articles');
-        articles = (await articlesResponse.json()).articles;
-        
-        for (const article of articles) {
-            if (!sectionNames[article.section_id]) {
-                const sectionResponse = await fetch(`https://newenki.zendesk.com/api/v2/help_center/fr/sections/${article.section_id}`);
-                const sectionData = await sectionResponse.json();
-                sectionNames[article.section_id] = sectionData.section;
-                console.log(sectionNames[article.section_id]);
-            }
+  var loadArticles = async function() {
+      const articlesResponse = await fetch('https://support.enki-home.com/api/v2/help_center/fr/articles');
+      articles = (await articlesResponse.json()).articles;
+      
+      for (const article of articles) {
+          if (!sectionNames[article.section_id]) {
+              const sectionResponse = await fetch(`https://support.enki-home.com/api/v2/help_center/fr/sections/${article.section_id}`);
+              const sectionData = await sectionResponse.json();
+              sectionNames[article.section_id] = sectionData.section;
+              console.log(sectionNames[article.section_id]);
+          }
 
-            let category_id = sectionNames[article.section_id].category_id; 
+          let category_id = sectionNames[article.section_id].category_id; 
 
-            if (!categoryNames[category_id]) {
-                const categoryResponse = await fetch(`https://newenki.zendesk.com/api/v2/help_center/fr/categories/${category_id}`);
-                const categoryData = await categoryResponse.json();
-                categoryNames[article.section_id] = categoryData.category.name;
-            }
-        }
+          if (!categoryNames[category_id]) {
+              const categoryResponse = await fetch(`https://support.enki-home.com/api/v2/help_center/fr/categories/${category_id}`);
+              const categoryData = await categoryResponse.json();
+              categoryNames[article.section_id] = categoryData.category.name;
+          }
+      }
 
-        articles.forEach(article => {
-        article.label_names.forEach(label => {
-                labelCounts[label] = (labelCounts[label] || 0) + 1;
-            });
-        });
-    }
-
+      articles.forEach(article => {
+          article.label_names.forEach(label => {
+              labelCounts[label] = (labelCounts[label] || 0) + 1;
+          });
+      });
+  }
 
   // Cette fonction est appelée lors du chargement du composant pour charger les labels
   async function loadLabels() {
-  try {
-    const response = await fetch('https://newenki.zendesk.com/api/v2/help_center/articles/labels', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Basic ${encodedCredentials}`
-      },
-    });
+      try {
+          const response = await fetch('https://support.enki-home.com/api/v2/help_center/articles/labels', {
+              method: 'GET',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Basic ${encodedCredentials}`
+              },
+          });
 
-    if (response.ok) {
-      const labels = await response.json(); // Mettez à jour l'état des labels avec les données de l'API
-      console.log(labels); // Afficher les labels dans la console pour débogage
-    } else {
-      // Gérer les erreurs de réponse ici
-      console.error("Erreur lors du chargement des labels: ", response.statusText);
-    }
-  } catch (error) {
-    console.error("Erreur lors de la connexion à l'API", error);
-  }
-}
-
-let labelCounts = {}; // { labelName: count, ... }
-
-// Étape 2: Ajout de Label
-    async function addLabel(articleId, labelName) {
-        if (labelName.trim() === '') {
-            console.log("Le label ne peut pas être vide");
-            return;
-        }
-        
-        try {
-      const response = await fetch(`https://newenki.zendesk.com/api/v2/help_center/articles/${articleId}/labels`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Basic ${encodedCredentials}`
-        },
-        body: JSON.stringify({ label: { name: labelName } })
-      });
-
-      if (response.ok) {
-        // Si le label est ajouté avec succès, mettez à jour l'interface utilisateur
-        let newLabel = await response.json(); // Supposons que l'API renvoie le nouveau label
-        articles = articles.map((article) => {
-          if (article.id === articleId) {
-            // Ajoutez le nouveau label à la liste des labels
-            return { ...article, label_names: [...article.label_names, newLabel] };
+          if (response.ok) {
+              const labelsData = await response.json(); // Mettez à jour l'état des labels avec les données de l'API
+              labels = labelsData.labels; // Assurez-vous que vous stockez les labels correctement
+              console.log(labels); // Afficher les labels dans la console pour débogage
+          } else {
+              // Gérer les erreurs de réponse ici
+              console.error("Erreur lors du chargement des labels: ", response.statusText);
           }
-          return article;
-        });
-      } else {
-        // Gérer les erreurs de l'API ici
-        console.error("Erreur lors de l'ajout du label");
+      } catch (error) {
+          console.error("Erreur lors de la connexion à l'API", error);
       }
-    } catch (error) {
-      console.error("Erreur lors de la connexion à l'API", error);
-    }
-
-    // Cachez l'input et réinitialisez l'état d'édition
-    editingLabel[articleId] = false;
-    // Il est important de mettre à jour Svelte sur les changements d'état
-    editingLabel = { ...editingLabel };
   }
 
+  let labelCounts = {}; // { labelName: count, ... }
 
-    let editingLabel = {}; // { articleId: boolean }
+  // Étape 2: Ajout de Label
+  async function addLabel(articleId, labelName) {
+      if (labelName.trim() === '') {
+          console.log("Le label ne peut pas être vide");
+          return;
+      }
+      
+      try {
+          const response = await fetch(`https://support.enki-home.com/api/v2/help_center/articles/${articleId}/labels`, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Basic ${encodedCredentials}`
+              },
+              body: JSON.stringify({ label: { name: labelName } })
+          });
 
-    function promptDeleteLabel(articleId, labelName) {
-    labelToDelete = { articleId, labelName };
-    const deleteModal = new bootstrap.Modal(document.getElementById('deleteLabelModal'));
-    deleteModal.show();
+          if (response.ok) {
+              // Si le label est ajouté avec succès, mettez à jour l'interface utilisateur
+              let newLabelData = await response.json(); // L'API retourne peut-être un objet label
+              let newLabelName = newLabelData.label.name; // Récupérez le nom du label
+              articles = articles.map((article) => {
+                  if (article.id === articleId) {
+                      // Ajoutez le nouveau label à la liste des labels
+                      return { ...article, label_names: [...article.label_names, newLabelName] };
+                  }
+                  return article;
+              });
+              // Mettez à jour labelCounts
+              labelCounts[labelName] = (labelCounts[labelName] || 0) + 1;
+          } else {
+              // Gérer les erreurs de l'API ici
+              console.error("Erreur lors de l'ajout du label");
+          }
+      } catch (error) {
+          console.error("Erreur lors de la connexion à l'API", error);
+      }
+
+      // Cachez l'input et réinitialisez l'état d'édition
+      editingLabel[articleId] = false;
+      // Il est important de mettre à jour Svelte sur les changements d'état
+      editingLabel = { ...editingLabel };
+  }
+
+  let editingLabel = {}; // { articleId: boolean }
+
+  function promptDeleteLabel(articleId, labelName) {
+      labelToDelete = { articleId, labelName };
+      const deleteModal = new bootstrap.Modal(document.getElementById('deleteLabelModal'));
+      deleteModal.show();
   }
 
   async function confirmDelete() {
-    if (labelToDelete) {
-      await deleteLabel(labelToDelete.articleId, labelToDelete.labelName);
-      // Vous pourriez vouloir cacher la modal après la suppression
-      const deleteModal = bootstrap.Modal.getInstance(document.getElementById('deleteLabelModal'));
-      deleteModal.hide();
-    }
-  }
-
-    async function deleteLabel(articleId, labelName) {
-    const label = labels.find(l => l.name === labelName);
-    if (!label) {
-      console.error("Label non trouvé");
-      return;
-    }
-
-    try {
-      const response = await fetch(`https://newenki.zendesk.com/api/v2/help_center/articles/${articleId}/labels/${label.id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Basic ${encodedCredentials}`
-        },
-      });
-
-      if (response.ok) {
-        // Supprimez le label de l'affichage après la suppression réussie
-        articles = articles.map(article => {
-          if (article.id === articleId) {
-            return {
-              ...article,
-              label_names: article.label_names.filter(name => name !== labelName)
-            };
-          }
-          return article;
-        });
-      } else {
-        // Gérer les erreurs de l'API ici
-        console.error("Erreur lors de la suppression du label");
+      if (labelToDelete) {
+          await deleteLabel(labelToDelete.articleId, labelToDelete.labelName);
+          // Vous pourriez vouloir cacher la modal après la suppression
+          const deleteModal = bootstrap.Modal.getInstance(document.getElementById('deleteLabelModal'));
+          deleteModal.hide();
       }
-    } catch (error) {
-      console.error("Erreur lors de la connexion à l'API", error);
-    }
   }
 
+  async function deleteLabel(articleId, labelName) {
+      const label = labels.find(l => l.name === labelName);
+      if (!label) {
+          console.error("Label non trouvé");
+          return;
+      }
 
+      try {
+          const response = await fetch(`https://support.enki-home.com/api/v2/help_center/articles/${articleId}/labels/${label.id}`, {
+              method: 'DELETE',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Basic ${encodedCredentials}`
+              },
+          });
+
+          if (response.ok) {
+              // Supprimez le label de l'affichage après la suppression réussie
+              articles = articles.map(article => {
+                  if (article.id === articleId) {
+                      return {
+                          ...article,
+                          label_names: article.label_names.filter(name => name !== labelName)
+                      };
+                  }
+                  return article;
+              });
+              // Mettez à jour labelCounts
+              labelCounts[labelName] -= 1;
+          } else {
+              // Gérer les erreurs de l'API ici
+              console.error("Erreur lors de la suppression du label");
+          }
+      } catch (error) {
+          console.error("Erreur lors de la connexion à l'API", error);
+      }
+  }
+
+  // Variables réactives pour le filtrage et la pagination
+  $: filteredArticles = articles.filter(article => 
+      (labelFilter === 'all' || 
+      (labelFilter === 'duplicate' && article.label_names.some(label => labelCounts[label] > 1))) &&
+      (!selectedCategory || categoryNames[article.section_id] === selectedCategory) && 
+      (!selectedSection || (sectionNames[article.section_id] && sectionNames[article.section_id].name === selectedSection))
+  );
+
+  // Réinitialiser la page actuelle lorsque les filtres changent
+  $: currentPage = 1;
+
+  // Calcul du nombre total de pages
+  $: totalPages = Math.ceil(filteredArticles.length / pageSize);
+
+  // Articles à afficher sur la page actuelle
+  $: paginatedArticles = filteredArticles.slice((currentPage - 1) * pageSize, currentPage * pageSize);
 </script>
 
 {#if showModal}
@@ -211,7 +232,6 @@ let labelCounts = {}; // { labelName: count, ... }
   </div>
 </div>
 {:else}
-
 
 <table>
     <thead>
@@ -232,22 +252,18 @@ let labelCounts = {}; // { labelName: count, ... }
                         <option value={section.name}>{section.name}</option>
                     {/each}
                 </select>
-                <th>
-                    <select bind:value={labelFilter}>
-                        <option value="all">Tous les Labels</option>
-                        <option value="duplicate">Doublons</option>
-                    </select>
-                </th>
+            </th>
+            <th>
+                <select bind:value={labelFilter}>
+                    <option value="all">Tous les Labels</option>
+                    <option value="duplicate">Doublons</option>
+                </select>
+            </th>
             <th>URL</th>
         </tr>
     </thead>
     <tbody>
-        {#each articles.filter(article => 
-            (labelFilter === 'all' || 
-            (labelFilter === 'duplicate' && article.label_names.some(label => labelCounts[label] > 1))) &&
-            (!selectedCategory || categoryNames[article.section_id] === selectedCategory) && 
-            (!selectedSection || (sectionNames[article.section_id] && sectionNames[article.section_id].name === selectedSection))
-        ) as article}
+        {#each paginatedArticles as article}
             <tr>
                 <!-- Affiche le titre de l'article -->
                 <td>{article.title}</td>
@@ -299,6 +315,21 @@ let labelCounts = {}; // { labelName: count, ... }
         {/each}
     </tbody>
 </table>
+
+<!-- Contrôles de pagination -->
+<div class="pagination">
+    <button on:click={() => currentPage = Math.max(1, currentPage - 1)} disabled={currentPage === 1}>Précédent</button>
+    {#each Array(totalPages) as _, index}
+        <button 
+            on:click={() => currentPage = index + 1} 
+            class:active={currentPage === index + 1}
+        >
+            {index + 1}
+        </button>
+    {/each}
+    <button on:click={() => currentPage = Math.min(totalPages, currentPage + 1)} disabled={currentPage === totalPages}>Suivant</button>
+</div>
+
 {/if}
 
 <style>
@@ -379,5 +410,21 @@ i.bi-trash {
     .label {
 
     }
+
+    .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+}
+
+    .pagination button {
+    margin: 0 5px;
+    padding: 5px 10px;
+}
+
+    .pagination button.active {
+    font-weight: bold;
+    text-decoration: underline;
+}
 
 </style>
